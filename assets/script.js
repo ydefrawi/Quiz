20360d11bec35fc4f8efa99d3beda470b04a007b
ydefrawi <ydefrawi@gmail.com>
2021-03-27 21:53:20 -0500

    Improved buttons

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,29 @@
-// //  Selects carousel element
-// var carousel = document.querySelector(".carouselbox");
-
-// // Selects buttons using their parent carousel element
-// var next = carousel.querySelector(".next");
-// var prev = carousel.querySelector(".prev");
-
-
 var startBtn = document.getElementById("startQuiz");
+var answerButtons = document.getElementById("multipleChoice")
 var currentQuestion = document.querySelector("#newQuestion")
+var questions = ["Question 1?","Question2?","Question 3?", "Question 4?", "Question 5?"];
 var q1Answers = ["Walter PPK", "AR-15", "Famas", "Sdar"];
-var q1 = "What's my name?"
+
+var totalQuestions=questions.length;
+console.log(totalQuestions)
 //number of quiz questions
-var numQuestions;
+var questionNumber;
 //Sets number of seconds in timer 
 var secondsLeft=75;
-
 //sets a space for the timer to appear at the top right 
 var countSpace = document.querySelector("#countSpace");
-console.log(countSpace)
+
+//hides answer buttons div initially 
+answerButtons.style.display = "none";
 
 startBtn.addEventListener("click", function() {
   setTime();
   startBtn.style.display = "none";
+  answerButtons.style.display = "block";
+  for (questionNumber=0; questionNumber<totalQuestions; questionNumber++){
+    currentQuestion.textContent = questions[questionNumber];
+    
+  }
 })
 
 
